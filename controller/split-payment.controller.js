46003e91206ce1e56427c4b1648fe23727b8ff16
Yusuf Musa <volunux@gmail.com>
2022-07-21 05:09:42 +0100

    sixth commit

--- controller/split-payment.controller.js
+++ controller/split-payment.controller.js
@@ -5,30 +5,6 @@ module.exports = {
   'service': service ,
 
   'compute': function (req, res, next) {
-    var split = {
-    "ID": 1308,
-    "Amount": 12580,
-    "Currency": "NGN",
-    "CustomerEmail": "anon8@customers.io",
-    "SplitInfo": [
-        {
-            "SplitType": "FLAT",
-            "SplitValue": 45,
-            "SplitEntityId": "LNPYACC0019"
-        },
-        {
-            "SplitType": "RATIO",
-            "SplitValue": 3,
-            "SplitEntityId": "LNPYACC0011"
-        },
-        {
-            "SplitType": "PERCENTAGE",
-            "SplitValue": 3,
-            "SplitEntityId": "LNPYACC0015"
-        }
-      ]
-    };
-
     let results = module.exports.service.processTransaction(req.body);
     res.status(200).send(results);
   }
